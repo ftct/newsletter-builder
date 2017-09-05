f50af792ce5697a4b25df7f84edaa572b9c32eb7
ftct <hi5@fallenthoughts.com>
2017-09-05 08:08:01 +0200

    remove hardcoded paths

--- vendors/cleanup/cleanup.php
+++ vendors/cleanup/cleanup.php
@@ -1,6 +1,11 @@
 <?php
 
-$inputFilePath = "./output/content_ready.html";
+validateParametersCount($argv);
+
+$inputFilePath = $argv[1];
+$outputFilePath = $argv[2];
+
+validateInputData($inputFilePath);
 
 $content = file_get_contents($inputFilePath);
 
@@ -12,4 +17,24 @@ $contentOut = str_replace("</html>", "", $contentOut);
 $contentOut = str_replace("<body", "<div", $contentOut);
 $contentOut = str_replace("</body>", "</div>", $contentOut);
 
-file_put_contents($inputFilePath, $contentOut);
+file_put_contents($outputFilePath, $contentOut);
+
+function validateParametersCount($parameters)
+{
+    if (count($parameters) != 3) {
+        echo "Wrong number of parameters.
+        Usage: cleanup.php input_path.html output_path.html
+          - input_path.html - path to file with input html
+          - output_path.html - path to file where to save output file
+    ";
+        exit(1);
+    }
+}
+
+function validateInputData($inputFilePath)
+{
+    if (!file_exists($inputFilePath)) {
+        echo "Can not find html file.";
+        exit(1);
+    }
+}
