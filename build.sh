0bb9e6b7a8b443f2570d14517a112e21c6a41e28
ftct <hi5@fallenthoughts.com>
2017-09-05 08:03:29 +0200

    remove hardcoded paths

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ echo "Injecting styles inline."
 php vendors/css_flat/convert_to_flat_css.php ./output/content.html ./news.css ./output/content_inline.html
 echo "  Done: content_inline.html created."
 echo "Injecting images inside html."
-php vendors/image_inline/inject_images.php
-php vendors/cleanup/cleanup.php
+php vendors/image_inline/inject_images.php ./output/content_inline.html ./output/content_ready.html
+php vendors/cleanup/cleanup.php ./output/content_ready.html ./output/content_ready.html
 echo "  Done: content_ready.html created."
 
 echo "Generated \n";
--- vendors/image_inline/inject_images.php
+++ vendors/image_inline/inject_images.php
@@ -1,6 +1,11 @@
 <?php
 
-$inputFilePath = "./output/content_inline.html";
+validateParametersCount($argv);
+
+$inputFilePath = $argv[1];
+$outputFilePath = $argv[2];
+
+validateInputData($inputFilePath);
 
 $content = file_get_contents($inputFilePath);
 
@@ -8,9 +13,30 @@ $doc = new DOMDocument();
 $doc->loadHTML($content);
 $tags = $doc->getElementsByTagName('img');
 foreach ($tags as $tag) {
-    $imgUrl = "./images/" . $tag->getAttribute('src'); //Little path hack for prototype
-    $imageType = mime_content_type($imgUrl);     //getImageType($imgUrl);
+    $imgUrl = './images/' . $tag->getAttribute('src');
+    $imageType = mime_content_type($imgUrl);
     $inlineImage = base64_encode(file_get_contents($imgUrl));
     $tag->setAttribute('src', "data:$imageType;base64," . $inlineImage);
 }
-$doc->save("./output/content_ready.html");
+$doc->save($outputFilePath);
+
+
+function validateParametersCount($parameters)
+{
+    if (count($parameters) != 3) {
+        echo "Wrong number of parameters.
+        Usage: inject_images.php input_path.html output_path.html
+          - input_path.html - path to file with input html
+          - output_path.html - path to file where to save output file
+    ";
+        exit(1);
+    }
+}
+
+function validateInputData($inputFilePath)
+{
+    if (!file_exists($inputFilePath)) {
+        echo "Can not find html file.";
+        exit(1);
+    }
+}
